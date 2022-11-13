501155c7c5468e31e7c875b7736bde2b0a57fda6
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-11-13 09:34:22 +0200

    [sources]  Export HTTP request details to Starlark functions.

--- sources/hello.star
+++ sources/hello.star
@@ -1,4 +1,6 @@
 
-def main () :
+def main (_method, _headers, _path, _query) :
+	print (repr (_method), repr (_path), repr (_query))
+	print (repr (_headers))
 	return "hello world!"
 
--- sources/lib.rs
+++ sources/lib.rs
@@ -14,15 +14,43 @@ mod star {
 		
 		eval::Evaluator,
 		
+		values::Value,
+		values::dict::Dict,
+		values::Heap,
+		
+		collections::SmallMap,
+		
 	};
 }
+
+use ::starlark::{
+		
+		values::AllocValue as _,
+		values::ValueLike as _,
+		
+	};
+
+
 
 
 use ::hyper_simple_server as hss;
 
-use ::vrl_errors::*;
+use hss::{
+		
+		ResponseExtBuild as _,
+		
+	};
 
 pub use hss::MainResult;
+
+
+
+
+use ::vrl_errors::*;
+
+
+
+
 
 
 
@@ -66,17 +94,56 @@ pub fn main () -> MainResult {
 	
 	let _handler = move |_request : hss::Request<hss::Body>| {
 			
-			let _empty_module = star::Module::new ();
-			let mut _evaluator = star::Evaluator::new (&_empty_module);
+			let _request_module = star::Module::new ();
+			
+			let _heap = _request_module.heap ();
+			
+			let _request_method_value = _heap.alloc_str (_request.method () .as_str ()) .to_value ();
+			let _request_path_value = _heap.alloc_str (_request.uri () .path ()) .to_value ();
+			let _request_query_value = if let Some (_query) = _request.uri () .query () {
+					_heap.alloc_str (_query).to_value ()
+				} else {
+					star::Value::new_none ()
+				};
+			
+			let _request_headers_value = {
+					let _headers_http = _request.headers ();
+					let mut _headers_map = star::SmallMap::with_capacity (_headers_http.keys_len ());
+					for _name_http in _headers_http.keys () {
+						let _values_http = _headers_http.get_all (_name_http) .into_iter () .collect::<Vec<_>> ();
+						let _name_value = _heap.alloc_str (_name_http.as_str ()) .to_value ();
+						let _name_hashed = _name_value.get_hashed () .anyerr () .else_wrap (0x676d34f9) ?;
+						fn _http_to_star <'v> (_http : &hss::HeaderValue, _heap : &'v star::Heap) -> star::Value<'v> {
+							let _http = String::from_utf8_lossy (_http.as_bytes ());
+							_heap.alloc_str (&_http) .to_value ()
+						}
+						if _values_http.len () == 1 {
+							let _value = _http_to_star (&_values_http[0], _heap);
+							_headers_map.insert_hashed (_name_hashed, _value);
+						} else {
+							let _values = _heap.alloc_list_iter (_values_http.into_iter () .map (|_http| _http_to_star (_http, _heap))) .to_value ();
+							_headers_map.insert_hashed (_name_hashed, _values);
+						}
+					}
+					let _headers_dict = star::Dict::new (_headers_map);
+					_headers_dict.alloc_value (_heap)
+				};
+			
+			let mut _evaluator = star::Evaluator::new (&_request_module);
 			let _main = _main.clone ();
 			let _main = _main.value ();
-			let _outcome = _evaluator.eval_function (_main, &[], &[]) .unwrap ();
+			let _outcome = _evaluator.eval_function (_main, &[_request_method_value, _request_headers_value, _request_path_value, _request_query_value], &[]) .unwrap ();
+			
+			if let Some (_text) = _outcome.unpack_str () {
+				let _text = _text.to_owned ();
+				let _response = hss::Response::new_200_with_text (_text);
+				return Ok (_response);
+			}
 			
 			let _body = format! ("{}", _outcome);
-			
 			let _body = hss::Body::from (_body);
-			let mut _response = hss::Response::new (_body);
-			Ok (_response)
+			let _response = hss::Response::new_200_with_text (_body);
+			return Ok (_response);
 		};
 	
 	let _handler = hss::HandlerFnSync::from (_handler);
